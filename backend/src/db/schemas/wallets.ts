c50f4a4df0e8619230fcf6720afab692d3c5c0dd
Albert-Chyb <albert7773000@gmail.com>
2026-08-21 19:49:19 +0200

    Add wallets table

---
+++ backend/src/db/schemas/wallets.ts
@@ -0,0 +1,22 @@
+import {
+  integer,
+  timestamp,
+  snakeCase,
+  text,
+  check,
+} from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
+
+export const wallets = snakeCase.table(
+  'wallets',
+  {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: text().notNull(),
+    createdAt: timestamp().notNull().defaultNow(),
+    initialBalance: integer().notNull().default(0),
+  },
+  (table) => [
+    check('wallets_name_not_blank', sql`length(trim(${table.name})) > 0`),
+    check('wallets_name_max_length', sql`length(trim(${table.name})) <= 50`),
+  ],
+);
