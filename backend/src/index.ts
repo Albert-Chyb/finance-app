9011d8aeb19b3d81b84887bf6b93edcf1909bfd5
Albert-Chyb <albert7773000@gmail.com>
2026-08-21 19:09:02 +0200

    Add Drizzle.js

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,9 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+const db = drizzle(process.env.DATABASE_URL!);
 
 const app = new Hono();
 
